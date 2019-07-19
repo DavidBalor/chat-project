f7430363d51a51bb2c1b6a58ac288c34e198e094
DavidBalor <david.balderrama.dev@gmail.com>
2019-07-19 14:04:36 -0600

    second video middle

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -4,6 +4,27 @@
   const messageForm = document.querySelector('#message-form')
   const messageBox = document.querySelector('#message')
   const chat = document.querySelector('#chat')
+
+  //Getting Elements from the nickname form
+  const nickForm = document.querySelector('#nickForm')
+  const nickname = document.querySelector('#nickname')
+  const nickError = document.querySelector('#nick-error')
+
+  //Card Users
+  const usernames = document.querySelector('#usernames')
+
+  nickForm.addEventListener('submit', ev => {
+    ev.preventDefault()
+    socket.emit('new user', nickname.value, data => {
+      if (data) {
+        document.querySelector('#nickWrap').style.display = 'none'
+        document.querySelector('#contentWrap').style.display = 'block'
+      } else {
+        let message = '<div class="alert alert-danger">That username already exists</div>'
+        nickError.innerHTML = message
+      }
+    })
+  })
   
   messageForm.addEventListener('submit', ev => {
     ev.preventDefault()
--- src/sockets.js
+++ src/sockets.js
@@ -1,8 +1,22 @@
 module.exports = (io) => {
+  let nicknames = []
   io.on('connection', socket => {
     console.log('new user connected')
+    //Send Message Event
     socket.on('send message', data => {
       io.sockets.emit('new message', data)
     })
+
+    //New user Event
+    socket.on('new user', (nick,cb) => {
+      if (nicknames.indexOf(nick) > -1) {
+        cb(false)
+      } else {
+        socket.nickname = nick
+        nicknames.push(nick)
+        cb(true)
+      }
+    })
+
   })
 }
